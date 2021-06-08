2269581c7acaff57a28f3374452aebb655866856
haifei <haifei.zhang@powercoders.org>
2021-06-08 16:59:29 +0200

    Add random-book exercise

---
+++ 03-react/01-random-book/src/main.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const appDiv = document.getElementById("app");
+const books: string[] = [
+  "Anna Karenina",
+  "To Kill a Mockingbird",
+  "The Great Gatsby",
+  "One Hundred Years of Solitude",
+  "A Passage to India",
+  "Invisible Man",
+  "Don Quixote",
+  "Beloved",
+  "Mrs. Dalloway",
+  "Things Fall Apart",
+  "Jane Eyre",
+  "The Color Purple",
+];
+
+if (!(appDiv instanceof HTMLDivElement)) {
+  throw new Error("No div with id 'app' found");
+}
+
+const getRandomBook = (array: string[]): string => {
+  const number = Math.floor(Math.random() * array.length);
+  return array[number];
+};
+const getRandomColor = (): string => {
+  const keys = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += keys[Math.round(Math.random() * 15)];
+  }
+  return color;
+};
+
+const App = (): JSX.Element => {
+  return (
+    <p style={{ backgroundColor: getRandomColor() }}>{getRandomBook(books)}</p>
+  );
+};
+
+ReactDOM.render(<App />, appDiv);
