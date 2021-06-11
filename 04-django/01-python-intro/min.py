c9063d2ce6ef0e1f3c5823a7865d46cc14ff73cb
haifei <haifei.zhang@powercoders.org>
2021-06-11 18:05:08 +0200

    Raise Error

--- 04-django/01-python-intro/min.py
+++ 04-django/01-python-intro/min.py
@@ -1,11 +1,13 @@
-def min(array): 
-    try:
-        min_number = array[0]   
+def min(array):     
+    min_number = array[0]
+    if (array==[]):
+        raise IndexError('The array is empty')
+    else:
         for x in array:
             if x <= min_number:min_number = x      
-            else: min_number = min_number
-        return min_number
-    except IndexError as e:
-        print('array is empty')
+        else: min_number = min_number
+    return min_number
+
+    
 print(min([6,-2,7,3,-3]))
 print(min([]))
