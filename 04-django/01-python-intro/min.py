a923aedb48337fb0086adab3b8e624a81e1abcfa
haifei <haifei.zhang@powercoders.org>
2021-06-11 17:24:30 +0200

    Modify empty array case

--- 04-django/01-python-intro/min.py
+++ 04-django/01-python-intro/min.py
@@ -1,7 +1,11 @@
 def min(array): 
-    min_number =array[0]   
-    for x in array:
-        if x <= min_number:min_number= x      
-        else: min_number=min_number
-    return min_number
+    try:
+        min_number = array[0]   
+        for x in array:
+            if x <= min_number:min_number = x      
+            else: min_number = min_number
+        return min_number
+    except IndexError as e:
+        print('array is empty')
 print(min([6,-2,7,3,-3]))
+print(min([]))
