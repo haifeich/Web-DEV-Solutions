c548464d04f2f19a6ab1a404d40b275248d5eda5
haifei <haifei.zhang@powercoders.org>
2021-06-12 12:15:40 +0200

    Method without recursion

--- 04-django/01-python-intro/password_with_constraints.py
+++ 04-django/01-python-intro/password_with_constraints.py
@@ -8,11 +8,25 @@ def generator():
 
 def check():
     good = generator()
-    if ('-'in good or '_'in good):
-        for x in good:
-            if x.isnumeric():return good
-            else: return check()       
-    else: return check()
+    # Recursion
+    # if ('-'in good or '_'in good):
+    #     for x in good:
+    #         if x.isnumeric():return good
+    #         else: return check()       
+    # else: return check()    
+    i = 0
+    while i < len(good):       
+        if (good[i] =='-' or good[i]== '_'):
+            for x in range(len(good)):
+                if good[x].isnumeric():return good
+                elif x == len(good)-1:
+                    good = generator()
+                    i = -1                
+        elif i == len(good)-1:
+            good = generator()
+            i = -1
+        i = i + 1
+        
 
 password = ''.join(check())
 print(password)
