9e65d6e43f29ef2978a20111e60640f86469caa2
haifei <haifei.zhang@powercoders.org>
2021-06-14 11:46:07 +0200

    Preparation for unsplash-search

---
+++ 03-react/05-unsplash-search/src/main.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { useState } from "react";
+import * as ReactDOM from "react-dom";
+
+const appDiv = document.getElementById("app");
+
+if (!(appDiv instanceof HTMLDivElement)) {
+  throw new Error("No div with id 'app' found");
+}
+
+const App = (): JSX.Element => {
+  return <></>;
+};
+
+ReactDOM.render(<App />, appDiv);
